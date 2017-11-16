8fd6de1decb928abec1ffbe59c7f096808d6aa62
SzymiJimi <rekas1@tlen.pl>
2017-11-16 01:57:10 +0100

    Initial commit

---
+++ src/main/java/com/inzynieria/insurance/InsuranceApplication.java
@@ -0,0 +1,12 @@
+package com.inzynieria.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceApplication.class, args);
+	}
+}
