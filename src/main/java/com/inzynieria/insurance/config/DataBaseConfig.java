7b5da0d59bd5ac09cfddd22718618775ab92d3da
SzymiJimi <rekas1@tlen.pl>
2017-11-22 17:55:42 +0100

    Pierwszy controller

--- src/main/java/com/inzynieria/insurance/config/DataBaseConfig.java
+++ src/main/java/com/inzynieria/insurance/config/DataBaseConfig.java
@@ -1,30 +1,40 @@
 package com.inzynieria.insurance.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataBaseConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean(name = "mysqlDB")
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         //MySQL database we are using
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");//change url
-        dataSource.setUsername("mysql");//change userid
-        dataSource.setPassword("admin");//change pwd
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
 
-        //H2 database
-        /*
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");*/
         return dataSource;
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/static/");
+        resolver.setSuffix(".html");
+        return resolver;
     }
 }
---
+++ src/main/java/com/inzynieria/insurance/controller/DataBaseController.java
@@ -0,0 +1,12 @@
+package com.inzynieria.insurance.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DataBaseController {
+
+    public void create()
+    {
+        System.out.println("HAAAAAAAAAAAAAALO: wyświetlam informacje");
+    }
+}
