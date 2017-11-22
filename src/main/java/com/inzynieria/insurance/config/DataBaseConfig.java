f17e6e9ed9270a7c8fa64fa211cc9dd69a663d1a
SzymiJimi <rekas1@tlen.pl>
2017-11-22 14:18:00 +0100

    DataBase configuration

---
+++ src/main/java/com/inzynieria/insurance/config/DataBaseConfig.java
@@ -0,0 +1,30 @@
+package com.inzynieria.insurance.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfig {
+
+    @Bean(name = "mysqlDB")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        //MySQL database we are using
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");//change url
+        dataSource.setUsername("mysql");//change userid
+        dataSource.setPassword("admin");//change pwd
+
+        //H2 database
+        /*
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");*/
+        return dataSource;
+    }
+}
