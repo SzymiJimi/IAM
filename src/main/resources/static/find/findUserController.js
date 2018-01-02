c09c62a77b615d9ad666b014e5381cfe7870d39f
SzymDu <szymek23q@gmail.com>
2018-01-02 17:20:12 +0100

    Wyswietlanie danych oferty

--- src/main/resources/static/find/findUserController.js
+++ src/main/resources/static/find/findUserController.js
@@ -1,36 +1,40 @@
-(function () {
-    'use strict';
-    var app= angular.module('app',[]);
-    app.controller('findUserController', ['$scope','$http','findUserService', function($scope, $http, findUserService) {
 
 
-    $scope.submitForm = function(){
-        var url = "http://localhost:8090/user/find";
+var app = angular.module('app',[]);
+app.controller('findUserController', [
+    '$scope',
+    '$http' ,
+    'findUserService',
+    '$window',
+    function($scope, $http, findUserService, $window ) {
 
-        var config = {
-            headers : {
-                Accept: undefined
-            }
+
+        $scope.submitForm = function(){
+            var url = "http://localhost:8090/user/find";
+
+            var config = {
+                headers : {
+                    Accept: undefined
+                }
+            };
+
+            var value = $scope.name;
+            $scope.postResult=[];
+
+            findUserService.findUserInDB(url, value, config).then(function (result) {
+                angular.copy(result, $scope.postResult);
+                if($scope.postResult.length===0)
+                {
+                    $scope.response="Nie znaleziono użytkownika w bazie!";
+                }else{
+                    $scope.response="Znaleziono użytkownika!";
+                }
+            });
         };
 
-        var value = $scope.name;
-        $scope.postResult=[];
-
-        findUserService.findUserInDB(url, value, config).then(function (result) {
-            angular.copy(result, $scope.postResult);
-            if($scope.postResult.length===0)
-            {
-                $scope.response="Nie znaleziono użytkownika w bazie!";
-            }else{
-            $scope.response="Znaleziono użytkownika!";
-            }
-        });
-    };
-
-    $scope.selectUser = function(id){
-         url = "http://localhost:8090/client/show/"+id;
-         $window.location.href =url;
-    }
-}])
-})();
+        $scope.selectUser = function(id){
+            url = "http://localhost:8090/client/show/"+id;
+            $window.location.href =url;
+        }
+    }]);
 
